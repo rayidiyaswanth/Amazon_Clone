645b2776a79020452b04743eb625fa9e0b3cdfe6
rayidiyaswanth <rayidi46@gmail.com>
2025-06-02 01:50:35 +0530

    refined the fuctions of add to cart button

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -23,7 +23,7 @@ products.forEach(product => {
             $${(product.priceCents / 100).toFixed(2)}
           </div>
 
-          <div class="product-quantity-container">
+          <div class="product-quantity-container js-product-quantity">
             <select>
               <option selected value="1">1</option>
               <option value="2">2</option>
@@ -46,7 +46,7 @@ products.forEach(product => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart" 
-            data-product-id="${product.id}" data-product-price="${(product.priceCents / 100).toFixed(2)}">
+            data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -58,6 +58,7 @@ document.querySelector('.js-products').innerHTML += html; ;
 document.querySelectorAll('.js-add-to-cart').forEach(button => {
   button.addEventListener('click', event => {
     const productId = button.dataset.productId;
+    const quantity = Number(button.parentElement.querySelector('.js-product-quantity select').value);
     let machingItem;
     cart.forEach(item => {
       if (item.Id === productId) {
@@ -66,13 +67,29 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
     });
 
     if (machingItem) {
-      machingItem.quantity++;
+      machingItem.quantity+= quantity;
     } 
     else{
       cart.push({
         Id: productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
+
+    let cartQuantity = 0;
+    cart.forEach(item => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    const addedToCartElem = button.parentElement.querySelector('.added-to-cart');
+    addedToCartElem.classList.add('visible');
+
+    if (addedToCartElem.hideTimeoutId) {
+      clearTimeout(addedToCartElem.hideTimeoutId);
+    }
+    addedToCartElem.hideTimeoutId = setTimeout(() => {
+      addedToCartElem.classList.remove('visible');
+      addedToCartElem.hideTimeoutId = null;
+    }, 2000);
   })
 });
