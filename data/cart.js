2cc293c968ea83b5bdb6f6a281d6dfc4e515f857
rayidiyaswanth <rayidi46@gmail.com>
2025-06-02 15:13:42 +0530

    Loading cart to & from localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,9 +1,11 @@
-export const cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = [  
   {Id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2,},
   {Id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1,}
-];
+  quantity: 1,}];
+}
 
 export function addToCart(productId, quantity) {
   let machingItem;
@@ -22,6 +24,11 @@ export function addToCart(productId, quantity) {
       quantity: quantity,
     });
   }
+  saveCart();
+}
+
+export function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function removeFromCart(productId) {
@@ -29,5 +36,6 @@ export function removeFromCart(productId) {
     if (CartItem.Id === productId) {
       cart.splice(cart.indexOf(CartItem), 1);
     }
-  })
+  });
+  saveCart();
 }
